24038f51b6c7ab1adc46a363e72a5a64e57d3f28
brijesh <brijesh20jivani@gmail.com>
2023-09-17 22:51:03 +0530

    did some formatting

--- header.h
+++ header.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-uint8_t sample_maze[9][17]={"##### #########\n",
+const uint8_t sample_maze[9][17]={"##### #########\n",
                             "#   #       # #\n",
                             "### # ##### # #\n",
                             "#   #   #     #\n",
--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "header.h"
 
 const int rows=9,cols=17;
@@ -8,12 +5,11 @@ const int r_rows = rows, r_cols = cols-2;
 
 void main()
 {
-    uint8_t **maze_p = calloc(rows,sizeof(uint8_t*));
-    for(int i=0 ; i<rows ; i++)
-    maze_p[i] = calloc(cols,sizeof(uint8_t));
-    
-    for (int i = 0; i < rows; i++)
+    uint8_t **const maze_p = calloc(rows,sizeof(uint8_t*));
+    for(int i=0 ; i<rows ; i++){
+        maze_p[i] = calloc(cols,sizeof(uint8_t));
         memcpy(maze_p[i],sample_maze[i],cols);
+    }        
 
     /* copy original maze to maze_sol to modify for solution*/
     uint8_t **maze_sol = calloc(rows,sizeof(uint8_t*));
@@ -21,9 +17,8 @@ void main()
         maze_sol[i] = calloc(cols,sizeof(uint8_t));
         memcpy(maze_sol[i],maze_p[i],cols);
     }
-    print_maze(maze_p);    
+    print_maze(maze_p);
 }
-
 
 void print_maze(uint8_t **maze){
     for(uint8_t i = 0; i<rows; i++)
